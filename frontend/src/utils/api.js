ccae01687e66fab2dad11821b3f31d3336b0287f
Nicolò Cavalli <nicolo.cavalli95@gmail.com>
2024-02-15 22:56:05 +0100

    test

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 //==============================
 // Consts
 //==============================
-const BASE_URL = 'http://127.0.0.1:8000/api/';
+const BASE_URL = 'https://nicolocavalli.com/api/';
+// const BASE_URL = 'http://127.0.0.1:8000/api/';
 
 
 //==============================
